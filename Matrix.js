80d64490ba649f47261468156448a301c36ecc1d
yshin4 <yshin4@lion.lmu.edu>
2017-04-08 14:31:47 -0700

    fixed matrix.js

--- Matrix.js
+++ Matrix.js
@@ -1,6 +1,13 @@
 class Matrix {
     constructor (matrixArray){
-        this.matrixArray = matrixArray;
+        let identityMatrix = [
+            [ 1, 0, 0, 0 ],
+            [ 0, 1, 0, 0 ],
+            [ 0, 0, 1, 0 ],
+            [ 0, 0, 0, 1 ]
+        ];
+
+        this.matrixArray = matrixArray || identityMatrix;
     }
 
     print(){
@@ -12,14 +19,15 @@ class Matrix {
 
     multiply(m2){
         let m1 = this.matrixArray;
+        m2 = m2.matrixArray;
         let m3 = [];
-        for (let i = 0; i < 4; i++){
+        for (let i = 0; i < m1[0].length; i++){
             m3[i] = [];
-            for (let j = 0; j < 4; j++){
-                m3[i][j] = m1[i][0] * m2.matrixArray[0][j] +
-                           m1[i][1] * m2.matrixArray[1][j] +
-                           m1[i][2] * m2.matrixArray[2][j] +
-                           m1[i][3] * m2.matrixArray[3][j];
+            for (let j = 0; j < m2[0].length; j++){
+                m3[i][j] = m1[i][0] * m2[0][j] +
+                           m1[i][1] * m2[1][j] +
+                           m1[i][2] * m2[2][j] +
+                           m1[i][3] * m2[3][j];
             }
         }
         return m3;
--- scene.js
+++ scene.js
@@ -10,22 +10,35 @@
         [0, 6, 2, 0],
         [3, 8, 1, 4],
         [1, 8, 5, 6]
-    ]
+    ];
 
     let matrixArray2 = [
+        [7],
+        [5],
+        [8],
+        [0]
+    ];
+
+    let matrixArray3 = [
         [7, 5, 8, 0],
         [1, 8, 2, 6],
         [9, 4, 3, 8],
         [5, 3, 7, 9]
-    ]
+    ];
+
+    let m = new Matrix();
+    console.log("m");
+    m.print();
 
     let m1 = new Matrix(matrixArray);
     let m2 = new Matrix(matrixArray2);
-    m1.print();
-    let m3 = new Matrix(m1.multiply(m2));
-    console.log("new ");
-    m3.print();
-
+    let m3 = new Matrix(matrixArray3);
+    let m4 = new Matrix(m1.multiply(m2));
+    let m5 = new Matrix(m1.multiply(m3));
+    console.log("m4");
+    m4.print();
+    console.log("m5");
+    m5.print();
 
     let objectArray = [];
     objectArray.push(sphere);
